dd2f369505438cb37dd67df61ee43b802cd66b08
Swetha <74365171+swepon@users.noreply.github.com>
2025-07-23 20:49:39 -0400

    Add code for Q1 and Q4

--- knn.py
+++ knn.py
@@ -45,6 +45,10 @@ def knn_impute_by_item(matrix, valid_data, k):
     # Implement the function as described in the docstring.             #
     #####################################################################
     acc = None
+
+    numbers = KNNImputer(n_neighbors=k)
+    mat = numbers.fit_transform(matrix.T) 
+    acc = sparse_matrix_evaluate(valid_data, mat.T)
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -67,7 +71,48 @@ def main():
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+    k_values = [1, 6, 11, 16, 21, 26]
+    user_accuracies = []
+    item_accuracies = []
+
+    # User-based collaborative filtering
+    for i in k_values:
+        accuracy = knn_impute_by_user(sparse_matrix, val_data, i)
+        user_accuracies.append(accuracy)
+
+    best_k_user = k_values[np.argmax(user_accuracies)]
+    test_accuracy = knn_impute_by_user(sparse_matrix, test_data, best_k_user)
+    
+    print("Best k: ", best_k_user)
+    print("Test accuracy: ", test_accuracy)
+    
+    # Plot for user
+    plt.figure(figsize=(15, 10))
+    plt.plot(k_values, user_accuracies, marker='o')
+    plt.xlabel("k")
+    plt.ylabel("Accuracy")
+    plt.title("KNN for User")
+    plt.grid(True)
+    plt.show()
+    
+    # Item-based collaborative filtering
+    for i in k_values:
+        accuracy = knn_impute_by_item(sparse_matrix, val_data, i)
+        item_accuracies.append(accuracy)
+
+    best_k_item = k_values[np.argmax(item_accuracies)]
+    test_accuracy_item = knn_impute_by_item(sparse_matrix, test_data, best_k_item)
+    print("Best k: ", best_k_item)
+    print("Test accuracy: ", test_accuracy_item)
+
+    # Plot for item
+    plt.figure(figsize=(15, 10))
+    plt.plot(k_values, item_accuracies, marker='o')
+    plt.xlabel("k")
+    plt.ylabel("Accuracy")
+    plt.title("KNN for Item")
+    plt.grid(True)
+    plt.show()
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
